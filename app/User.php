7baa03c33c067e7cc09825659609093e5d08a103
Pontus Kindblad <pontus@enklare.eu>
2015-03-11 01:22:51 +0100

    fixar lite

--- app/User.php
+++ app/User.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
-	use SammyK\LaravelFacebookSdk\SyncableGraphNodeTrait;
+//	use SammyK\LaravelFacebookSdk\SyncableGraphNodeTrait;
 	use Authenticatable, CanResetPassword;
 
 	/**
