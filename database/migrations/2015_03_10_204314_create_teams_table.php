28772c300dba48628a48965102dafa8f1f4fe0e5
Pontus Kindblad <pontus@enklare.eu>
2015-03-13 10:21:47 +0100

    fungerande första försök med FB

---
+++ database/migrations/2015_03_10_204314_create_teams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('teams', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('team_name',100);
+			$table->string('team_country',100);
+			$table->text('logo_path');
+			$table->text('country_flag_path');
+			$table->text('description');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('teams');
+	}
+
+}
