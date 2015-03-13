35753f125a38ec724297574af948ef7a10a4a332
Anton Kindblad <anton.kindblad@gmail.com>
2015-03-13 11:50:30 +0100

    rapid deploy testing

--- resources/views/canvas/login.blade.php
+++ resources/views/canvas/login.blade.php
@@ -12,7 +12,7 @@
             });
             FB.login(function(response) {
                 if (response.authResponse) {
-                    window.parent.location.reload();
+                    console.log(response);
                 } else {
                     console.log('User cancelled login or did not fully authorize.');
                 }
