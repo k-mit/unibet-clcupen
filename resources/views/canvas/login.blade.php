3b6b06d6c1253f2dbd830f3e31e60699920d066a
Anton Kindblad <anton.kindblad@gmail.com>
2015-03-13 11:20:07 +0100

    added login view

---
+++ resources/views/canvas/login.blade.php
@@ -0,0 +1,34 @@
+@extends('app')
+
+@section('content')
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '1445118565779083',
+                xfbml      : true,
+                version    : 'v2.2'
+            });
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    console.log('Welcome!  Fetching your information.... ');
+                    FB.api('/me', function(response) {
+                        console.log('Good to see you, ' + response.name + '.');
+                    });
+                } else {
+                    console.log('User cancelled login or did not fully authorize.');
+                }
+            },{scope: 'email'});
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+
+
+@endsection
