cc4e7cdd0b71d6069f1282bbcca1efb9cc7f92bd
Pontus Kindblad <pontus@enklare.eu>
2015-03-25 11:18:54 +0100

    Bet was missing

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -192,7 +192,7 @@ class FacebookController extends Controller {
 	 */
 	public function highscoreFriends($fbFriends) {
 
-		
+
 	}
 
 	/**
@@ -248,6 +248,7 @@ class FacebookController extends Controller {
 		/*		$bets = Bet::whereHas('match.round', function ($q) {
 					$q->where('rounds.id', '=', 1);
 				})->with(['match.result', 'user'])->get();*/
+		dd($bets);
 		foreach ($bets as $bet) {
 			echo $bet->match->result->goals_team1 . '<br>';
 			echo $bet->match->result->goals_team2 . '<br>';
