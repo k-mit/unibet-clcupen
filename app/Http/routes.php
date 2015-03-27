582d7a6c1b7e4646a7e9e533c185189b8805dd71
Pontus Kindblad <pontus@enklare.eu>
2015-03-27 21:05:01 +0100

    cleaned up routes with the help of the new auth.facebook middleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,173 +13,19 @@
 use \App\Http\Controllers\FacebookController;
 use SammyK\LaravelFacebookSdk\LaravelFacebookSdk;
 
-// Generate a login URL
-Route::get('/apps', function () {
-    return view('canvas.index');
-});
+Route::get('/', function () {return '';});
 Route::any('facebook/canvas', [
     'uses' => 'facebookController@viewCanvas',
     'middleware' => 'auth.facebook',
 ]);
-Route::get('/facebook/login', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
-    // Send an array of permissions to request
-    $login_url = $fb->getLoginUrl(['email']);
-    return redirect($login_url);
-    // Obviously you'd do this in blade :)
-//    echo '<a href="' . $login_url . '">Login with Facebook</a>';
-});
-Route::get('/facebook/friends', 'FacebookController@getFacebookFriends');
 
-Route::get('/facebook/datafeed', 'FacebookController@datafeed');
-Route::get('/testsavebet', function () {
-    return view('testsavebet');
-});
-
-
-
-Route::get('/', function () {return '';});
-
-Route::get('/calculateRound', ['middleware' => 'auth', 'uses' => 'FacebookController@calculateRound']);
-Route::post('/saveBet', ['middleware' => 'auth', 'uses' => 'FacebookController@saveBet']);
-Route::post('/admin/notifyAll', ['middleware' => 'auth', 'uses' => 'AdminController@notifyAll']);
-Route::post('/admin/notifyPersons', ['middleware' => 'auth', 'uses' => 'AdminController@notifyPersons']);
-Route::get('/admin/notifications', ['middleware' => 'auth', 'uses' => 'AdminController@notifications']);
-Route::get('/admin/notificationsPersons', ['middleware' => 'auth', 'uses' => 'AdminController@notificationsPersons']);
-Route::get('/admin/snippets', ['middleware' => 'auth', 'uses' => 'AdminController@snippets']);
-Route::post('/admin/saveNotification', ['middleware' => 'auth', 'uses' => 'AdminController@saveNotification']);
-Route::post('/admin/saveSnippet', ['middleware' => 'auth', 'uses' => 'AdminController@saveSnippet']);
-
-
-// Endpoint that is redirected to after an authentication attempt
-Route::get('facebook/connected', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
-    // Obtain an access token.
-    try {
-        $token = $fb->getAccessTokenFromRedirect();
-
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
-
-    // Access token will be null if the user denied the request
-    // or if someone just hit this URL outside of the OAuth flow.
-    if (!$token) {
-        // Get the redirect helper
-        $helper = $fb->getRedirectLoginHelper();
-
-        if (!$helper->getError()) {
-            abort(403, 'Unauthorized action.');
-        }
-
-        // User denied the request
-        dd(
-            $helper->getError(),
-            $helper->getErrorCode(),
-            $helper->getErrorReason(),
-            $helper->getErrorDescription()
-        );
-    }
-
-    if (!$token->isLongLived()) {
-        // OAuth 2.0 client handler
-        $oauth_client = $fb->getOAuth2Client();
-
-        // Extend the access token.
-        try {
-            $token = $oauth_client->getLongLivedAccessToken($token);
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            dd($e->getMessage());
-        }
-    }
-
-    $fb->setDefaultAccessToken($token);
-
-    // Save for later
-    Session::put('fb_user_access_token', (string)$token);
-
-    // Get basic info on the user from Facebook.
-    try {
-        $response = $fb->get('/me?fields=id,name,email,user_friends');
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
-
-    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-    $facebook_user = $response->getGraphUser();
-
-    // Create the user if it does not exist or update the existing entry.
-    // This will only work if you've added the SyncableGraphNodeTrait to your User model.
-    $user = App\User::createOrUpdateGraphNode($facebook_user);
-
-    // Log the user into Laravel
-    Auth::login($user);
-
-    return redirect('/')->with('message', 'Successfully logged in with Facebook');
-
-}, []);
-
-Route::get('facebook/callback', function (SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb) {
-    // Obtain an access token.
-    try {
-        $token = $fb->getAccessTokenFromRedirect();
-
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
-
-    // Access token will be null if the user denied the request
-    // or if someone just hit this URL outside of the OAuth flow.
-    if (!$token) {
-        // Get the redirect helper
-        $helper = $fb->getRedirectLoginHelper();
-
-        if (!$helper->getError()) {
-            abort(403, 'Unauthorized action.');
-        }
-
-        // User denied the request
-        dd(
-            $helper->getError(),
-            $helper->getErrorCode(),
-            $helper->getErrorReason(),
-            $helper->getErrorDescription()
-        );
-    }
-
-    if (!$token->isLongLived()) {
-        // OAuth 2.0 client handler
-        $oauth_client = $fb->getOAuth2Client();
-
-        // Extend the access token.
-        try {
-            $token = $oauth_client->getLongLivedAccessToken($token);
-        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-            dd($e->getMessage());
-        }
-    }
-
-    $fb->setDefaultAccessToken($token);
-
-    // Save for later
-    Session::put('fb_user_access_token', (string)$token);
-
-    // Get basic info on the user from Facebook.
-    try {
-        $response = $fb->get('/me?fields=id,name,email,user_friends');
-    } catch (Facebook\Exceptions\FacebookSDKException $e) {
-        dd($e->getMessage());
-    }
-
-    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-    $facebook_user = $response->getGraphUser();
-
-    // Create the user if it does not exist or update the existing entry.
-    // This will only work if you've added the SyncableGraphNodeTrait to your User model.
-    $user = App\User::createOrUpdateGraphNode($facebook_user);
-
-    // Log the user into Laravel
-    Auth::login($user);
-
-    return redirect('/')->with('message', 'Successfully logged in with Facebook');
-
-});
+Route::get('/calculateRound', ['middleware' => 'auth.facebook', 'uses' => 'FacebookController@calculateRound']);
+Route::post('/saveBet', ['middleware' => 'auth.facebook', 'uses' => 'FacebookController@saveBet']);
+Route::post('/admin/notifyAll', ['middleware' => 'auth.facebook', 'uses' => 'AdminController@notifyAll']);
+Route::post('/admin/notifyPersons', ['middleware' => 'auth.facebook', 'uses' => 'AdminController@notifyPersons']);
+Route::get('/admin/notifications', ['middleware' => 'auth.facebook', 'uses' => 'AdminController@notifications']);
+Route::get('/admin/notificationsPersons', ['middleware' => 'auth.facebook', 'uses' => 'AdminController@notificationsPersons']);
+Route::get('/admin/snippets', ['middleware' => 'auth', 'auth.facebook' => 'AdminController@snippets']);
+Route::post('/admin/saveNotification', ['middleware' => 'auth.facebook', 'uses' => 'AdminController@saveNotification']);
+Route::post('/admin/saveSnippet', ['middleware' => 'auth', 'auth.facebook' => 'AdminController@saveSnippet']);
 
