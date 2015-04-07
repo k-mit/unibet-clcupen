2894057e68d3fdc3dd6941f528931a3d32a97f68
Anton Kindblad <anton@k-mit.se>
2015-04-07 11:31:31 +0200

    added moved facebook for canvas into canvas template

--- resources/views/canvas/index.blade.php
+++ resources/views/canvas/index.blade.php
@@ -283,6 +283,41 @@
         </main>
     </div>
     @if ($errors->any())
-        {{dd($errors)}}
+        {{--{{dd($errors)}}--}}
     @endif
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '1445118565779083',
+                xfbml      : true,
+                version    : 'v2.2',
+                cookie     : true
+            });
+            FB.getLoginStatus(function(response) {
+                if (response.status === 'connected') {
+                    // the user is logged in and has authenticated your
+                    // app, and response.authResponse supplies
+                    // the user's ID, a valid access token, a signed
+                    // request, and the time the access token
+                    // and signed request each expire
+                    var uid = response.authResponse.userID;
+                    var accessToken = response.authResponse.accessToken;
+                    console.log('doooit!')
+                } else if (response.status === 'not_authorized') {
+                    // the user is logged in to Facebook,
+                    // but has not authenticated your app
+                } else {
+                    // the user isn't logged in to Facebook.
+                }
+            });    };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
 @endsection
