140b74b2ebe77043ee1ea77a080cde4d5ced0818
desdichado <pontus@k-mit.se>
2015-04-14 15:42:42 +0000

    new column for rounds only

---
+++ database/migrations/2015_04_14_151232_rounds_update.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RoundsUpdate extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('rounds', function($table) {
+			$table->dateTime('match_close');
+		});
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
